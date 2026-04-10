3fb308912e41bef2a46fda0f73cba37b4757ff37
Infinitewumbo <millareslepatrick@gmail.com>
2026-04-10 03:12:37 -0400

    implement AuthGuard to protect employee routes from unauthenticated access

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { EmployeeList } from './components/employee-list/employee-list';
 import { Signup } from './components/signup/signup';
 import { Login } from './components/login/login';
+import { authGuard } from './auth/auth-guard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'signup', component: Signup },
-  { path: 'employees', component: EmployeeList },
+  { 
+    path: 'employees', 
+    component: EmployeeList,
+    canActivate: [authGuard]
+   },
   { path: '', redirectTo: '/login', pathMatch: 'full' } 
 ];
