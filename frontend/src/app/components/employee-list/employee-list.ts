df11c3cb3b9fc2e71fe703ad782b04d3b63420be
Infinitewumbo <millareslepatrick@gmail.com>
2026-04-09 23:50:02 -0400

    fix: set correct graphql endpoint uri in appConfig

--- frontend/src/app/components/employee-list/employee-list.ts
+++ frontend/src/app/components/employee-list/employee-list.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmployeeService } from '../../services/employee';
 
 @Component({
   selector: 'app-employee-list',
-  standalone: true, 
-  imports: [CommonModule], 
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './employee-list.html',
   styleUrl: './employee-list.css',
 })
-export class EmployeeList { }
+export class EmployeeList implements OnInit {
+  employees: any[] = [];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.employeeService.getEmployees().subscribe((result: any) => {
+      this.employees = result.data?.getAllEmployees || [];
+    });
+  }
+}
